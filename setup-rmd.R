301160a4598ea54e2db920e37455d32b655a62d4
tereom <teresa.ortiz.mancera@gmail.com>
2019-11-21 12:59:06 -0600

    Add cache to set_up

--- setup-rmd.R
+++ setup-rmd.R
@@ -3,7 +3,8 @@ knitr::opts_chunk$set(
     comment = "#>",
     collapse = TRUE,
     fig.align = "center", 
-    error = FALSE
+    warning = FALSE, 
+    cache=TRUE
 )
 comma <- function(x) format(x, digits = 2, big.mark = ",")
 ggplot2::theme_set(ggplot2::theme_light())
